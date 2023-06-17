1202231221b48998aab0bdaa3333aaa17ddf58e9
Moon-edu <edu.moonduck@gmail.com>
2023-06-17 17:35:48 +0900

    Delete solution

--- homework/q2_insert_employee_table.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO employee values('A00001', 'Male',	'Moon',	'10-199, Gang-nam, Seoul',	1,	'C00001',	30,	'Senior engineer');
-INSERT INTO employee values('B00100', 'Female',	'Sun',	'587/8, Gwan-ak, Seoul', 2,	'B00102',	25,	'Associate marketer');
-INSERT INTO employee values('A08771', 'Others',	'Peach',	'203-3, Guro, Seoul',	1,	'C00001',	26,	'Junior engineer');
-INSERT INTO employee values('C00129', 'Male',	'Alex',	'20-331, Bundang, Gyonggi',	3,	'C00002',	40,	'Director');
-INSERT INTO employee values('C00001', 'Male',	'Lion',	'53-3, Namyang-ju, Gyonghi',	1,	'C00000',	55,	'CTO');
-INSERT INTO employee values('C00002', 'Others',	'Cindy',	'100, Jong-ro, Seoul',	3,	'C00000',	52,	'Director');
-INSERT INTO employee values('B00102', 'Female',	'Ran',	'290-10, Gwanghwamun, Seoul',	2,	'C00000',	45,	'Director');
-INSERT INTO employee values('C00000', 'Male',	'K',	'1010, Sung-soo, Seoul',	null, null,	51,	'CEO');
--- homework/q3_create_visit_log_table.sql
+++
@@ -1,6 +0,0 @@
-create table visit_log(
-	visitor varchar(6) null,
-	enter timestamp not null,
-	out timestamp,
-	purpose varchar(50)
-)
