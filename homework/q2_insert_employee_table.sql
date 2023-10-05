8750ed89c5d57be693e8dc357e2903216355b8ae
쏘 <besthersoy@gmail.com>
2023-10-05 20:56:27 +0900

    Week2 과제 제출_v3
    Senior Engineer => Senior engineer

--- homework/q2_insert_employee_table.sql
+++ homework/q2_insert_employee_table.sql
@@ -19,7 +19,7 @@
 --/*Q2*/
 insert into employee (emp_id, gender, name, address, department, manager, age, position)
 values
-  ('A00001', 'Male'  , 'Moon' ,'10-199, Gang-nam, Seoul'    , 1, 'C00001', 30 , 'Senior Engineer')
+  ('A00001', 'Male'  , 'Moon' ,'10-199, Gang-nam, Seoul'    , 1, 'C00001', 30 , 'Senior engineer')
  ,('B00100', 'Female', 'Sun'  ,'587/8, Gwan-ak, Seoul'      , 2, 'B00102', 25 , 'Associate marketer')
  ,('A08771', 'Others', 'Peach','203-3, Guro, Seoul'         , 1, 'C00001', 26 , 'Junior engineer')
  ,('C00129', 'Male'  , 'Alex' ,'20-331, Bundang, Gyonggi'   , 3, 'C00002', 40 , 'Director' )
